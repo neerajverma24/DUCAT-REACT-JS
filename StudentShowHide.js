e86d692269b22dee158e23d761c4262a478e79dc
Neeraj Kumar <122043968+neerajverma24@users.noreply.github.com>
2024-03-27 16:56:10 +0530

    DUCAT REACT JS
    this repo has only src folder...

---
+++ StudentShowHide.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+
+function StudentShowHide() {
+    const [state, setState] = useState(true)
+
+    function showHideFunc() {
+        // console.log("Paragraph  show-hide... ")
+        setState(!state);
+    }
+    return (
+        <div>
+            <h3>Student Show-Hide</h3>
+            {state ? <p>Hello ! This is a Paragraph..</p> : " Hidden! Click to Show "}
+
+            {/* way 1  : Direct */}
+            {/* <button onClick={() => setState(!state)}>Show-Hide</button>  */}
+
+            {/* way 2 :  with function*/}
+            &nbsp; &nbsp;
+            <button onClick={() => showHideFunc()}>Show-Hide</button>
+        </div>
+    )
+}
+
+export default StudentShowHide;
